0009057015a7fe5e839aed42995a9accfd7b21a3
iamxiaoma <395487290@qq.com>
2018-01-06 17:31:00 +0800

    更新
    调整 eslintrc 验证规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,12 +17,25 @@ module.exports = {
   // add your custom rules here
   rules: {
     'semi': [2, 'never'],
-    'indent': ['error', 2],
+    //'semi': [0],
+    //'indent': ['error', 'tab'],
+    'indent': 'off',
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-tabs': 'off',
+    'no-empty': 'off',
+    'no-multi-spaces': 'off',
+    'no-trailing-spaces': 'off',
+    'padded-blocks': [2, 'never'],
+    'no-mixed-spaces-and-tabs': 'off',
+    'no-unused-vars': 0,
+    'operator-linebreak': 'off',
+    'comma-dangle': 0,
+    'comma-style': 0,
+    'comma-spacing': 0
   }
 }
